d55dca593ff1626701416854cbb2d0b669f66c0f
Muhammad-ibn-Abdullah <rblxgamer12312@protonmail.com>
2020-12-16 15:22:07 -0500

    Added comments

--- pointer_arithmetic.cpp
+++ pointer_arithmetic.cpp
@@ -6,6 +6,7 @@ int main()
     const int NSTRINGS = 5;
     string texts[NSTRINGS] = {"one", "two", "three", "four", "five"};
 
+    //Create a pointer and change where it is pointing to by arithmetic.
     string *ptr = texts;
     ptr += 3;
     cout << *ptr << endl;
@@ -14,9 +15,11 @@ int main()
 
     cout << endl;
 
+    //Set a specific pointer location by using the address operator.
     string *ptr_end = &texts[NSTRINGS-1];
     ptr = &texts[0];
 
+    //Comparing pointers.
     while (ptr != ptr_end) {
         cout << *ptr << endl;
         ptr++;
@@ -24,9 +27,11 @@ int main()
 
     ptr = &texts[0];
 
+    //Arithmetic between two pointers evaluates to an int value.
     long elements = (long)(ptr_end - ptr);
     cout << elements << endl << endl;
 
+    //Point the pointer to the middle element.
     ptr = &texts[0];
     ptr += NSTRINGS/2;
     cout << *ptr << endl;
